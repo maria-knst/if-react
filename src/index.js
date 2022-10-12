de503148210988a251abf0e6912fe6c095158155
Maria <konstantinovamaria25@gmail.com>
2022-10-12 15:52:38 +0300

    Added prettier to all files.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import App from './components/App/App'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
-import store, {persistor} from '../src/redux/store'
+import store, { persistor } from '../src/redux/store'
 
 window.React = React
 
@@ -13,9 +13,9 @@ const root = createRoot(element)
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-            <App />
-        </PersistGate>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,25 +1,24 @@
 import { createStore } from 'redux'
 import rootReduser from './rootReduser'
 import initialState from './initialState'
-import {persistReducer, persistStore} from "redux-persist";
+import { persistReducer, persistStore } from 'redux-persist'
 import LocalStorage from 'redux-persist/lib/storage'
 
 const persistConfig = {
-    key: 'root',
-    storage: LocalStorage,
-    blacklist: [],
-    whitelist: ['authorization', 'search'],
-};
+  key: 'root',
+  storage: LocalStorage,
+  blacklist: [],
+  whitelist: ['authorization', 'search'],
+}
 
 const persistedReduser = persistReducer(persistConfig, rootReduser)
 
 const store = createStore(
-    persistedReduser,
+  persistedReduser,
   initialState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
 
-export default store;
+export default store
 
-export const persistor = persistStore(store);
-
+export const persistor = persistStore(store)
